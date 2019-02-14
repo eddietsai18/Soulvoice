769dd2bb393935b9d8ef554391b1829e8cbbb42a
eddietsai <eddietsai@eddietsaide-MacBook-Pro.local>
2019-02-14 11:32:38 +0800

    init commit

--- Soulvoice/production_settings.py
+++ Soulvoice/production_settings.py
@@ -6,8 +6,8 @@ DATABASES = {
 }
 
 # Static asset configuration.
-# STATIC_ROOT = 'account/static'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'account/static'
+# STATIC_ROOT = 'staticfiles'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
