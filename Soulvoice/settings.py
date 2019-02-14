21a6940d07a91d2705f59d2c6f4f61f7fd7c6609
eddietsai <eddietsai@eddietsaide-MacBook-Pro.local>
2019-02-14 11:57:17 +0800

    init commit

--- Soulvoice/settings.py
+++ Soulvoice/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'Soulvoice.urls'
